273a2d788630fb01313bd6a75b742006d4d025f5
nguyendinhit319@gmail.com <Memilao1@@>
2020-12-27 21:23:53 +0700

    Version 0.0.1

---
+++ background.js
@@ -0,0 +1,17 @@
+console.log("background running");
+
+console.log("Content is running");
+
+function getPost() {
+    let post = document.getElementsByClassName("kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x c1et5uql");
+    console.log(post.length);
+}
+setTimeout(5000, getPost)
+console.log("kiin")
+// Bắt comment
+
+
+
+// kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x c1et5uql ii04i59q -> id bắt bài post
+// document.querySelectorAll('[class^="d2edcug0"]'); -> bắt seemore
+// kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x c1et5uql -> id bắt comment
---
+++ content.js
@@ -0,0 +1,11 @@
+console.log("Content is running");
+
+function getPost() {
+    let post = document.getElementsByClassName("kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x c1et5uql");
+    console.log(post.length);
+}
+setTimeout(5000, getPost)
+console.log("kiin")
+// Bắt comment
+
+
